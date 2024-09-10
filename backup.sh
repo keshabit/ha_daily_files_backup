6de4ea2300ecb96852a26e3dc1e2fd93800f1438
k <71192868+keshabit@users.noreply.github.com>
2024-09-10 23:45:14 +0800

    Backup script hourly change
    a. changed to hourly
    b. changed the daily folder to hourly
    c. changed the add-on app config to hourly
    d. removed the date field from the backup so that file that doesn't usually change 1 backup if not changed in the last 30 days 
    e. removed the name filter from deletion

--- backup.sh
+++ backup.sh
@@ -1,17 +1,23 @@
-# This script is created by keshabit to backup and save files that need frequent improvisions in the Home Assistant
+###################################################
+#        Basic backup script by Keshab            #
+###################################################
 #!/bin/bash
-_loc="/root/backup/daily/files/"
+#-------------------------------------------------
+_loc="/backup/hourly/files/"
+#-------------------------------------------------
 while read -r _line
 do 
   if [ -f "$_line" ]
   then
     _file=`echo $_line |awk -F "/" '{print $NF}'`
     cks=`cksum $_line| awk '{print $1}'` 
-    cp $_line $_loc$_file.$cks.$(date +"%Y%m%d")
+    cp $_line $_loc$_file.$cks
   else
     echo "Error: File $_line is not found!"
   fi
-done < "/backup/daily/config.files"
-
-# 30 days old backup auto cleanup
-find $_loc -name "*.20*" -type f -mtime +30 -exec rm {} \;
+done < "/backup/hourly/config.files"
+##################################################
+# 30 days old backup files auto cleanup
+#------------------------------------------------
+find $_loc -type f -mtime +30 -exec rm {} \;
+##################################################
